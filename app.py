3b6e4d4b0ca91164f8b4b5ca214278f23ccef191
tourpran <thepranavkrish04@gmail.com>
2022-01-03 22:21:38 +0530

    login signup done

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter.font import names
 import tkinter.messagebox as tkMessageBox
 import mysql.connector
+from PIL import ImageTk, Image
 
 # Tkinter functions:
 def login():
@@ -33,6 +34,14 @@ def login_failed():
     Label(screen1, text="").pack()
     Label(screen1, text="Your username and password is wrong check again.").pack()
 
+def fake_user():
+    screen1.destroy()
+    global screen
+    screen = Toplevel(root)
+    screen.title("fake user")
+    screen.geometry("900x200")
+
+    Label(screen, text="Username already found in Database.").pack()
 
 def register():
     global screen1
@@ -61,13 +70,22 @@ def register():
     Button(screen1, text="register", command=register_user).pack()
 
 def shop():
-    pass
+    for widgets in root.winfo_children():
+      widgets.destroy()
+    Label(root, text="Shop items").pack()
 
 def main_screen():
     global root 
     root = Tk()
     root.geometry("900x900")
     root.title("Retail Application")
+    
+    Label(text="").pack()
+    img = Image.open("login.png")
+    img = img.resize((100, 100), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    label = Label(root, image = img)
+    label.pack()
 
     Label(text="").pack()
     Button(root, text="Login", command=login).pack()
@@ -91,6 +109,10 @@ def register_user():
     password_info = passwd.get()
     address_info = address.get()
     mycursor = db.cursor()
+    mycursor.execute(f"SELECT * FROM users where name='{username_info}'")
+    mycursor.fetchall()
+    if mycursor.rowcount != 0:
+        fake_user()
     mycursor.execute(f"INSERT INTO users (name, password, address) VALUES ('{username_info}', '{password_info}', '{address_info}')")
     db.commit()
     screen1.destroy()
