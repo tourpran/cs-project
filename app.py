7850c30d2500ff8d2db300ccc9ccc9644fbe51df
tourpran <thepranavkrish04@gmail.com>
2022-01-04 17:11:10 +0530

    Completed project

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from tkinter.font import names
 import tkinter.messagebox as tkMessageBox
 import mysql.connector
 from PIL import ImageTk, Image
+from functools import partial
+
+global id
+global qty
+id = qty = 0
 
 # Tkinter functions:
 def login():
@@ -17,7 +22,7 @@ def login():
     global passwd
     passwd = StringVar()
 
-    Label(screen1, text="LOGIN").pack()
+    Label(screen1, text="LOGIN", font= ('Helvetica 15 underline')).pack()
     Label(screen1, text="").pack()
     Label(screen1, text="Username").pack()
     Entry(screen1, textvariable=username).pack()
@@ -72,7 +77,37 @@ def register():
 def shop():
     for widgets in root.winfo_children():
       widgets.destroy()
-    Label(root, text="Shop items").pack()
+    Label(root, text="Shop Items").pack()
+    Label(root, text="").pack()
+    items = get_items()
+    for product in items:
+        Label(root, text=f"{product[1]}").pack()
+
+        Button(root, text="add", command=partial(add_product, f"{product[1]}", f"{product[3]}")).pack()
+        Label(text="").pack()
+    Button(root, text="Total", command=bill).pack()
+
+def bill():
+    global total 
+    total = 0
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    Label(root, text="Totaling your items:").pack()
+    Label(root, text="").pack()
+    
+    mycursor = db.cursor()
+    mycursor.execute(f"select * from {username_info}")
+    items = mycursor.fetchall()
+    
+    Label(root, text=f"Product name: Quantity: Price").pack()
+    for product in items:
+        Label(root, text=f"{product[1]}: {product[2]}: {product[3]}").pack()
+        Label(root, text="").pack()
+        total += product[2]*product[3]
+
+    Label(root, text="--------------").pack()
+    Label(root, text=f"Grand total: {total}").pack()
+    Button(root, text="shop", command=shop).pack()
 
 def main_screen():
     global root 
@@ -95,14 +130,39 @@ def main_screen():
     root.mainloop()
 
 # Sql functions:
+def add_product(name, price):
+    global id, qty
+    mycursor = db.cursor()
+    mycursor.execute(f"select * from {username_info} where name='{name}'")
+    mycursor.fetchall()
+    if mycursor.rowcount == 0:
+        id += 1
+        mycursor.execute(f"insert into {username_info} values ({id}, '{name}', 1, {price})")
+        db.commit() 
+    else:
+        mycursor.execute(f"update {username_info} set qty=qty+1 where name='{name}'")
+        db.commit()
+    
+
 def mysqlconnect():
     global db
     db = mysql.connector.connect(user = 'pranav', host = 'localhost', password= "password", database = 'retailapp')
     print("Connected")
 
+def get_items():
+    mycursor = db.cursor()
+    mycursor.execute("select * from products")
+    record = mycursor.fetchall()
+    return record
+
 def initialise():
     mycursor = db.cursor()
-    mycursor.execute("CREATE TABLE users (name VARCHAR(255), password VARCHAR(255), address VARCHAR(255))")
+    try:
+        mycursor.execute("CREATE TABLE users (name VARCHAR(255), password VARCHAR(255), address VARCHAR(255))")
+        mycursor.execute("create table products(id int, name varchar(100), description varchar(100), price int)")
+        db.commit()
+    except:
+        pass
 
 def register_user():
     username_info = username.get()
@@ -118,6 +178,7 @@ def register_user():
     screen1.destroy()
 
 def login_user():
+    global username_info
     username_info = username.get()
     password_info = passwd.get()
     mycursor = db.cursor()
@@ -127,9 +188,15 @@ def login_user():
         screen1.destroy()
         login_failed()
     else:
+        try:
+            mycursor = db.cursor()
+            mycursor.execute(f"create table {username_info} (id int, name varchar(100), qty int, price int)")
+        except:
+            pass
         shop()
 
 # Initialisation
 mysqlconnect()
+initialise()
 main_screen()
     
